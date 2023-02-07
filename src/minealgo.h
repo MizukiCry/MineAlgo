88165da41594e781afab677a556681c83bca421a
MizukiCry <lanrtabe@qq.com>
2023-02-07 21:04:10 +0800

    Fix some bugs

--- src/minealgo.h
+++ src/minealgo.h
@@ -1,9 +1,11 @@
 #ifndef _MINEALGO_H
 #define _MINEALGO_H
 
-#include "ms_grid.h"
 #include "ms_board.h"
-#include "ms_solve.h"
 #include "ms_generate.h"
+#include "ms_grid.h"
+#include "ms_lib.h"
+#include "ms_solve.h"
+#include "ms_timer.h"
 
 #endif
